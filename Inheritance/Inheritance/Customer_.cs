3443fa9385bfe716a652825571338d94f0941620
NunoGitHub <nunoaapp22@hotmail.com>
2024-04-02 18:13:21 +0100

    access modifiers

---
+++ Inheritance/Inheritance/Customer_.cs
@@ -0,0 +1,22 @@
+﻿namespace AccessModifiers
+{
+    public class Customer_
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var rating = CalculateRating(excludeOrders: true);
+            if (rating == 0)
+                Console.WriteLine("Promote Level 1");
+            else
+                Console.WriteLine("Promote Level 2");
+        }
+
+        private int CalculateRating(bool excludeOrders)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var customer_ = new Customer_();
+            customer_.Promote();
+
+            var customer =  new Customer();
+            //cannot do this because the access modifier is internal, only accessible in the same library
+           // var rate = new Amazon.RateCalculator();
+        }
+    }
+}
