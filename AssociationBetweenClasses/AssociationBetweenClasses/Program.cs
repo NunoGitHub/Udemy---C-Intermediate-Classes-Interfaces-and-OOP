b59133d8b469b45871d15ccc35d05ba3805cb599
NunoGitHub <nunoaapp22@hotmail.com>
2024-04-02 11:45:35 +0100

    inheritance

---
+++ AssociationBetweenClasses/AssociationBetweenClasses/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Inheritance
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
+        }
+
+    }
+}
---
+++ AssociationBetweenClasses/AssociationBetweenClasses/Text.cs
@@ -0,0 +1,13 @@
+﻿namespace Inheritance
+{
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set; }
+        public int FontWeight { get; set; }
+
+        public void AddHyperLink(string url)
+        {
+            Console.WriteLine("We added a link to " + url);
+        }
+    }
+}
